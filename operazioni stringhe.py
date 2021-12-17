092821be3a1eb7936f7c8d1a483104f433a19fc9
mackalou99 <95908350+mackalou99@users.noreply.github.com>
2021-12-17 11:09:08 +0100

    Add files via upload

---
+++ operazioni stringhe.py
@@ -0,0 +1,11 @@
+
+m=['salve', 'Marcolino', 'io', 'piacere', 'sono']
+print(m[0], m[2],m[4],m[1],m[3])
+
+l=[len(m[0]),len(m[1]),len(m[2]),len(m[3]),len(m[4])]    
+x=sorted(l)
+print(x)
+k=sorted(m)
+print(k)
+
+    
---
+++ tuple.py
@@ -0,0 +1,7 @@
+
+tuple=("1" , "3" , "67", "0", "-6",)
+print(max(tuple))
+print(min(tuple))
+
+
+
